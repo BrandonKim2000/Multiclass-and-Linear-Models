8a96ae2406d5e63b7029be6f182c9fb14be8b508
Brandon Kim <bnkim3032@gmail.com>
2021-07-28 19:48:19 -0400

    First commit

---
+++ gd.py
@@ -0,0 +1,49 @@
+"""
+Generic implementation of gradient descent.
+"""
+import math
+
+import numpy
+import util
+from binary import *
+from util import *
+# Importing from what ELMS says I need to import
+from sklearn.tree import DecisionTreeClassifier
+import multiclass
+from datasets import *
+
+
+def gd(func, grad, x0, numIter, stepSize):
+    """
+    Perform gradient descent on some function func, where grad(x)
+    computes its gradient at position x.  Begin at position x0 and run
+    for exactly numIter iterations.  Use stepSize/sqrt(t+1) as a
+    step-size, where t is the iteration number.
+
+    We return the final solution as well as the trajectory of function
+    values.
+    """
+
+    # initialize current location
+    x = x0
+
+    # set up storage for trajectory of function values
+    trajectory = numpy.zeros(numIter + 1)
+    trajectory[0] = func(x)
+
+    # begin iterations
+    for iter in range(numIter):
+        # compute the gradient at the current location
+        g = grad(x)
+
+        # compute the step size
+        eta = stepSize/math.sqrt(iter+1)
+
+        # step in the direction of the gradient
+        x = x - eta*g
+
+        # record the trajectory
+        trajectory[iter + 1] = func(x)
+
+    # return the solution
+    return x, trajectory
